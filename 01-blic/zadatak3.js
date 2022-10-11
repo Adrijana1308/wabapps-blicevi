ff861f878fd4a0f26b7a0060a52b9c29e2a3d6e9
Adrijana1308 <adrijanababic0602@gmail.com>
2022-10-11 08:39:48 +0200

    3. zadatak

---
+++ 01-blic/zadatak3.js
@@ -0,0 +1,10 @@
+
+var func3 = function (array){
+    
+       var  x = product_data.sort(function(a, b) {
+          return /[A-Za-z]/.test(a.a) - /[A-Za-z]/.test(b.a) || a.a.charCodeAt(0) - b.a.charCodeAt(0)
+        });
+       
+    }
+   
+func3({a:"1"},{a:"B"},{a:"A"},{a:"J"},{a:"5"},{a:"2"})
